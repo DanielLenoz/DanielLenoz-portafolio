e55b152576168b5ff491e1cd66cfd4016f626593
DanielLenoz <danielstivenxx@outlook.com>
2022-09-03 23:10:09 -0500

    envio a repositorio de github

---
+++ carrusel.js
@@ -0,0 +1,42 @@
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const slider = document.querySelector(".slider");
+
+prev.addEventListener("click", () => {
+  slider.scrollLeft -= 700;
+});
+next.addEventListener("click", () => {
+  slider.scrollLeft += 700;
+});
+
+const prev2 = document.querySelector(".prev2");
+const next2 = document.querySelector(".next2");
+const slider2 = document.querySelector(".slider2");
+
+prev2.addEventListener("click", () => {
+  slider2.scrollLeft -= 700;
+});
+next2.addEventListener("click", () => {
+  slider2.scrollLeft += 700;
+});
+
+const prev3 = document.querySelector(".prev3");
+const next3 = document.querySelector(".next3");
+const slider3 = document.querySelector(".slider3");
+
+prev3.addEventListener("click", () => {
+  slider3.scrollLeft -= 700;
+});
+next3.addEventListener("click", () => {
+  slider3.scrollLeft += 700;
+});
+const prev4 = document.querySelector(".prev4");
+const next4 = document.querySelector(".next4");
+const slider4 = document.querySelector(".slider4");
+
+prev4.addEventListener("click", () => {
+  slider4.scrollLeft -= 700;
+});
+next4.addEventListener("click", () => {
+  slider4.scrollLeft += 700;
+});
