408191135135eb2a504caa1a11472e460be19983
DanielLenoz <danielstivenxx@outlook.com>
2023-01-23 12:08:09 -0500

    agregando nuevo curso extra

--- js/cursos_extra.js
+++ js/cursos_extra.js
@@ -3,6 +3,9 @@ const containerExtra = document.querySelector(".container-extra");
 const productListExtra = [];
 
 productListExtra.push({
+  image: "../img/img-cursos-extra/presupuesto-y-flujo-de-caja.jpg",
+});
+productListExtra.push({
   image: "../img/img-cursos-extra/marca-personal.jpg",
 });
 productListExtra.push({
