52c5a695fe1a32e8cd122d9f22028a756553d933
DanielLenoz <danielstivenxx@outlook.com>
2023-01-24 23:01:55 -0500

    agregando un nuevo curso de tecnologia

--- js/cursos_tecnologias.js
+++ js/cursos_tecnologias.js
@@ -3,6 +3,9 @@ const containerTech = document.querySelector(".container-tecnologia");
 const productListTech = [];
 
 productListTech.push({
+  image: "../img/img-tecnologia/closures-y-scope-en-javascript.jpg",
+});
+productListTech.push({
   image: "../img/img-tecnologia/practico-de-JavaScript.jpg",
 });
 productListTech.push({
