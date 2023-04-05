d3c510fc47fabbf7fb38ee0b3849784a5252cead
DanielLenoz <danielstivenxx@outlook.com>
2023-04-04 21:15:40 -0500

    terminando index.html iphone

---
+++ src/style/js/index.js/textIntro.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.auto-typed', {
+    strings: [
+        "Front-end Developer",
+        "Designer UI"
+    ],
+    typeSpeed: 100,
+    startDelay:300,
+    backSpeed: 50,
+    loop: true
+    
+})
