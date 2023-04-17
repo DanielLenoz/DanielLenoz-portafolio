2c9feb7efa42289edaa7589242aa94840004fbf4
DanielLenoz <danielstivenxx@outlook.com>
2023-04-17 12:12:04 -0500

    corrigiendo el nombre de las carpetas de las imagenes y agregando la carpeta img-projects

--- src/style/js/certificados/cursos_ingles.js
+++ src/style/js/certificados/cursos_ingles.js
@@ -2,85 +2,85 @@ const containerCaruelIngles = document.querySelector(".container-ingles");
 
 const inglesElementos = [
   {
-    image: "../../src/img/img-ingles/B1-requests-and-reflexive-pronouns.jpg",
+    image: "../../src/img/img-cursos-ingles/B1-requests-and-reflexive-pronouns.jpg",
   },
   {
-    image: "../../src/img/img-ingles/B1-prefixes-and-compound-adjectives.jpg",
+    image: "../../src/img/img-cursos-ingles/B1-prefixes-and-compound-adjectives.jpg",
   },
   {
-    image: "../../src/img/img-ingles/B1-adjectives-and-passive-voice.jpg",
+    image: "../../src/img/img-cursos-ingles/B1-adjectives-and-passive-voice.jpg",
   },
   {
-    image: "../../src/img/img-ingles/B1-relative-pronouns-and-clauses.jpg",
+    image: "../../src/img/img-cursos-ingles/B1-relative-pronouns-and-clauses.jpg",
   },
   {
-    image: "../../src/img/img-ingles/B1-present-perfect-and-prepositions.jpg",
+    image: "../../src/img/img-cursos-ingles/B1-present-perfect-and-prepositions.jpg",
   },
   {
-    image: "../../src/img/img-ingles/B1-infinitive-and-transitive-verbs.jpg",
+    image: "../../src/img/img-cursos-ingles/B1-infinitive-and-transitive-verbs.jpg",
   },
   {
-    image: "../../src/img/img-ingles/B1-connectors-and-preferences.jpg",
+    image: "../../src/img/img-cursos-ingles/B1-connectors-and-preferences.jpg",
   },
   {
-    image: "../../src/img/img-ingles/B1-expressions-of-time-and-quantity.jpg",
+    image: "../../src/img/img-cursos-ingles/B1-expressions-of-time-and-quantity.jpg",
   },
   {
-    image: "../../src/img/img-ingles/shopping.jpg",
+    image: "../../src/img/img-cursos-ingles/shopping.jpg",
   },
   {
-    image: "../../src/img/img-ingles/pronunciation.jpg",
+    image: "../../src/img/img-cursos-ingles/pronunciation.jpg",
   },
   {
-    image: "../../src/img/img-ingles/for-networking.jpg",
+    image: "../../src/img/img-cursos-ingles/for-networking.jpg",
   },
   {
-    image: "../../src/img/img-ingles/basic-writing.jpg",
+    image: "../../src/img/img-cursos-ingles/basic-writing.jpg",
   },
   {
-    image: "../../src/img/img-ingles/vocabulary-and-expressions.jpg",
+    image: "../../src/img/img-cursos-ingles/vocabulary-and-expressions.jpg",
   },
   {
-    image: "../../src/img/img-ingles/unsoled-mysteries.jpg",
+    image: "../../src/img/img-cursos-ingles/unsoled-mysteries.jpg",
   },
   {
-    image: "../../src/img/img-ingles/descriptions-and-comparatives.jpg",
-  },
-  {
-    image:
-      "../../src/img/img-ingles/A2-past-experiences-and-future-intentions.jpg",
-  },
-  {
-    image: "../../src/img/img-ingles/A2-conjunctions-and-superlatives.jpg",
+    image: "../../src/img/img-cursos-ingles/descriptions-and-comparatives.jpg",
   },
   {
     image:
-      "../../src/img/img-ingles/A2-present-continuous-and-prepositions.jpg",
+      "../../src/img/img-cursos-ingles/A2-past-experiences-and-future-intentions.jpg",
   },
   {
-    image: "../../src/img/img-ingles/A2-quantities-and-gerunds.jpg",
-  },
-  {
-    image: "../../src/img/img-ingles/A2-adverbs-and-nouns.jpg",
-  },
-  {
-    image: "../../src/img/img-ingles/A2-articles-and-model-verbs.jpg",
-  },
-  {
-    image: "../../src/img/img-ingles/A2-questions-and-answers.jpg",
-  },
-  {
-    image: "../../src/img/img-ingles/A1-dates,times-and-simple-expresiones.jpg",
-  },
-  {
-    image: "../../src/img/img-ingles/workshop-for-beginners.jpg",
-  },
-  {
-    image: "../../src/img/img-ingles/A1-for-beginners.jpg",
+    image: "../../src/img/img-cursos-ingles/A2-conjunctions-and-superlatives.jpg",
   },
   {
     image:
-      "../../src/img/img-ingles/A1-present-simple-and-common-vocabulary.jpg",
+      "../../src/img/img-cursos-ingles/A2-present-continuous-and-prepositions.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-ingles/A2-quantities-and-gerunds.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-ingles/A2-adverbs-and-nouns.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-ingles/A2-articles-and-model-verbs.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-ingles/A2-questions-and-answers.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-ingles/A1-dates,times-and-simple-expresiones.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-ingles/workshop-for-beginners.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-ingles/A1-for-beginners.jpg",
+  },
+  {
+    image:
+      "../../src/img/img-cursos-ingles/A1-present-simple-and-common-vocabulary.jpg",
   },
 ];
 
--- src/style/js/certificados/cursos_tecnologia.js
+++ src/style/js/certificados/cursos_tecnologia.js
@@ -4,84 +4,84 @@ const containerCaruelTecnologia = document.querySelector(
 
 const tecnologiaElementos = [
   {
-    image: "../../src/img/img-technologia/debugging-devtools.jpg",
+    image: "../../src/img/img-cursos-technologia/debugging-devtools.jpg",
   },
   {
-    image: "../../src/img/img-technologia/javaScript-engine-V8-navegador.jpg",
-  },
-  {
-    image:
-      "../../src/img/img-technologia/manipulacion-de-arrays-en-javaScript.jpg",
+    image: "../../src/img/img-cursos-technologia/javaScript-engine-V8-navegador.jpg",
   },
   {
     image:
-      "../../src/img/img-technologia/programacion-orientada-a-objetos-con-javaScript2.jpg",
+      "../../src/img/img-cursos-technologia/manipulacion-de-arrays-en-javaScript.jpg",
   },
   {
     image:
-      "../../src/img/img-technologia/programacion-orientada-a-objetos-con-javascript.jpg",
-  },
-  {
-    image: "../../src/img/img-technologia/programacion-orientada-a-objetos.jpg",
-  },
-  {
-    image: "../../src/img/img-technologia/closures-y-scope-en-javascript.jpg",
-  },
-  {
-    image: "../../src/img/img-technologia/practico-de-JavaScript.jpg",
-  },
-  {
-    image: "../../src/img/img-technologia/basico-Javascript.jpg",
-  },
-  {
-    image: "../../src/img/img-technologia/animaciones-css.jpg",
-  },
-  {
-    image: "../../src/img/img-technologia/transform-transition-css.jpg",
-  },
-  {
-    image: "../../src/img/img-technologia/diseño-web-con-css-grid-flexbox.jpg",
-  },
-  {
-    image: "../../src/img/img-technologia/css-grid-layout.jpg",
-  },
-  {
-    image: "../../src/img/img-technologia/maquetacion-en-css.jpg",
-  },
-  {
-    image: "../../src/img/img-technologia/css-grid-basico.jpg",
-  },
-  {
-    image: "../../src/img/img-technologia/maquetacion-mobile-first.jpg",
-  },
-  {
-    image: "../../src/img/img-technologia/practico-de-html-y-css.jpg",
-  },
-  {
-    image: "../../src/img/img-technologia/definitivo-de-html-y-css.jpg",
-  },
-  {
-    image: "../../src/img/img-technologia/practico-de-frontend-developer.jpg",
-  },
-  {
-    image: "../../src/img/img-technologia/frontend-developer.jpg",
-  },
-  {
-    image: "../../src/img/img-technologia/git-github.jpg",
+      "../../src/img/img-cursos-technologia/programacion-orientada-a-objetos-con-javaScript2.jpg",
   },
   {
     image:
-      "../../src/img/img-technologia/introduccion-a-la-terminal-y-linea-de-comandos.jpg",
+      "../../src/img/img-cursos-technologia/programacion-orientada-a-objetos-con-javascript.jpg",
   },
   {
-    image: "../../src/img/img-technologia/rework-windows.jpg",
+    image: "../../src/img/img-cursos-technologia/programacion-orientada-a-objetos.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-technologia/closures-y-scope-en-javascript.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-technologia/practico-de-JavaScript.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-technologia/basico-Javascript.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-technologia/animaciones-css.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-technologia/transform-transition-css.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-technologia/diseño-web-con-css-grid-flexbox.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-technologia/css-grid-layout.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-technologia/maquetacion-en-css.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-technologia/css-grid-basico.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-technologia/maquetacion-mobile-first.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-technologia/practico-de-html-y-css.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-technologia/definitivo-de-html-y-css.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-technologia/practico-de-frontend-developer.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-technologia/frontend-developer.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-technologia/git-github.jpg",
   },
   {
     image:
-      "../../src/img/img-technologia/fundamentos-de-ingenieria-de-software.jpg.jpg",
+      "../../src/img/img-cursos-technologia/introduccion-a-la-terminal-y-linea-de-comandos.jpg",
   },
   {
-    image: "../../src/img/img-technologia/introduccion-a-la-web.jpg",
+    image: "../../src/img/img-cursos-technologia/rework-windows.jpg",
+  },
+  {
+    image:
+      "../../src/img/img-cursos-technologia/fundamentos-de-ingenieria-de-software.jpg.jpg",
+  },
+  {
+    image: "../../src/img/img-cursos-technologia/introduccion-a-la-web.jpg",
   },
 ];
 
