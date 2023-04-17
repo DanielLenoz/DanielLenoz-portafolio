4f0de4dc9d2fe37019d8afda06fa76e032ef511a
DanielLenoz <danielstivenxx@outlook.com>
2023-04-17 12:42:31 -0500

    corriguiendo rutas y fotos

--- src/style/js/projects/projects.js
+++ src/style/js/projects/projects.js
@@ -8,6 +8,8 @@ const projectsElements = [
     contactImgDirection: "contact_img-left",
     imgDirection: "img-left",
     brilloPosition: "brillo-left",
+    linkesGithub: "https://github.com/DanielLenoz",
+    linksShare: "", 
     //** animacion scroll*/
     dataAos: "fade-up-right",
     //**contenido de los contenedores */
@@ -17,8 +19,8 @@ const projectsElements = [
     description:
       "proyecto de portafolio v1 la primera versión de una de mis creacion. siempre actualizando los proyectos para que sean la mejor version de sigo mismos",
 
-    imgDescktop: "../../src/img/img-desktop/portafolio-img-desktop.jpg",
-    imgIphone: "../../src/img/img-iphone/portafolio-img-iphone.jpg",
+    imgDescktop: "../../src/img/img-projects/img-desktop/portafolio-img-desktop.jpg",
+    imgIphone: "../../src/img/img-projects/img-iphone/portafolio-img-iphone.jpg",
   },
   {
     projectsDirection: "projects-right",
@@ -27,6 +29,8 @@ const projectsElements = [
     contactImgDirection: "contact_img-right",
     imgDirection: "img-right",
     brilloPosition: "brillo-right",
+    linkesGithub: "https://github.com/DanielLenoz",
+    linksShare: "", 
     //** animacion scroll*/
     dataAos: "flip-right",
     //**contenido de los contenedores */
@@ -37,8 +41,8 @@ const projectsElements = [
     description:
       "proyecto de portafolio v1 la primera versión de una de mis creacion. siempre actualizando los proyectos para que sean la mejor version de sigo mismos",
 
-    imgDescktop: "../../src/img/img-desktop/portafolio-img-desktop.jpg",
-    imgIphone: "../../src/img/img-iphone/portafolio-img-iphone.jpg",
+    imgDescktop: "../../src/img/img-projects/img-desktop/portafolio-img-desktop.jpg",
+    imgIphone: "../../src/img/img-projects/img-iphone/portafolio-img-iphone.jpg",
   },
 ];
 
@@ -75,6 +79,7 @@ projectsElements.forEach((the) => {
 
   const figureImg = document.createElement("figure");
   figureImg.classList.add(the.imgDirection);
+  figureImg.classList.add("img--projects")
   const pictureImg = document.createElement("picture");
   const sourceImg = document.createElement("source");
   sourceImg.setAttribute("media", "(min-width:945px)");
@@ -89,11 +94,18 @@ projectsElements.forEach((the) => {
 
   const containerIconsShare = document.createElement("section");
   containerIconsShare.classList.add("container_icons--share");
+  const linkesGithub = document.createElement("a");
+  linkesGithub.setAttribute('href', the.linkesGithub);
   const iconGithub = document.createElement("img");
   iconGithub.setAttribute("src", "../../src/img/assets/icons-redes/github.svg");
+  linkesGithub.appendChild(iconGithub)
+
+  const linksShare = document.createElement('a');
+  linksShare.setAttribute('href', the.linksShare)
   const iconShare = document.createElement("img");
   iconShare.setAttribute("src", "../../src/img/assets/icons-redes/share.svg");
-  containerIconsShare.append(iconGithub, iconShare);
+  linksShare.appendChild(iconShare)
+  containerIconsShare.append(linkesGithub, linksShare);
 
   //**concatenacion final left */
 
